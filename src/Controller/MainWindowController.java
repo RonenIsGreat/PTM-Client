ecd680499c3c6b0828f9ec94721a34bba5ff920a
aliceka495 <aliceka495@gmail.com>
2018-12-07 18:45:37 +0200

    added Canvas+MazeDisplayer

--- src/Controller/MainWindowController.java
+++ src/Controller/MainWindowController.java
@@ -1,5 +1,33 @@
 package Controller;
 
-public class MainWindowController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import View.MazeDisplayer;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class MainWindowController implements Initializable{
 	
+	int[][] mazeData= {
+			{1,1,1,1,1,1},	
+			{0,0,0,0,0,1},	
+			{1,1,1,0,0,1},	
+			{1,0,1,0,0,1},	
+			{1,0,1,0,0,1},	
+			{1,1,1,1,0,1},
+			{1,1,1,0,0,1},
+			{1,1,1,1,1,1},
+	};
+	
+	
+	@FXML
+	MazeDisplayer mazeDisplayer;
+	
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		mazeDisplayer.setMazeData(mazeData);
+		
+	}
 }
